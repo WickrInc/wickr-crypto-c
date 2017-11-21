e46bf2a31d9b96c24278b6c616bb310b46a193b2
Tom Leavy <tom@wickr.com>
2017-11-20 22:30:32 -0500

    Updated build system to correct CMake issues and build 3rd party libraries as CMake subprojects as git submodules

--- third-party/protobuf-c-configure.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-if test ! -f ./Makefile ; then
-    PKG_CONFIG_PATH=`pwd`/../../lib/pkgconfig
-    export PKG_CONFIG_PATH
-    ./configure --prefix=`pwd`/../.. CFLAGS="-g -O0" CXXFLAGS="-g -O0"
-fi
