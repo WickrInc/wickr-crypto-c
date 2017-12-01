4e07b7fd837e1cbc4f15f4338e7633518ec61fa3
mrolinek <michalrolinek@gmail.com>
2017-12-01 16:50:19 +0100

    Integrated tests, few bugfixes

---
+++ src/test/test_ed448_suite.h
@@ -0,0 +1,13 @@
+#ifndef test_ed448_suite_h
+#define test_ed448_suite_h
+
+#include "cspec.h"
+
+DEFINE_DESCRIPTION(ed448_signature_scheme)
+DEFINE_DESCRIPTION(ed448_sig_derive_public_key)
+DEFINE_DESCRIPTION(ed448_dh_derive_public_key)
+DEFINE_DESCRIPTION(ed448_dh_shared_secret)
+DEFINE_DESCRIPTION(ed448_shake256_raw)
+DEFINE_DESCRIPTION(ed448_shake256);
+
+#endif /* test_ed448_suite_h */
