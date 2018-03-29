8112b6e815b055fae4b189d620930f84a9f365c0
Tom Leavy <tom@wickr.com>
2018-03-29 18:22:51 -0400

    Added protocol version regression tests with static inputs

---
+++ test/test_protocol_version.h
@@ -0,0 +1,16 @@
+//
+//  test_protocol_version.h
+//  protobuf-c
+//
+//  Created by Tom Leavy on 3/29/18.
+//
+
+#ifndef test_protocol_version_h
+#define test_protocol_version_h
+
+#include <stdio.h>
+#include "cspec.h"
+
+DEFINE_DESCRIPTION(protocol_support_regression_tests);
+
+#endif /* test_protocol_version_h */
