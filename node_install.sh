86a659f44c008d49ec31131b19c9a5f557189fb4
Tom Leavy <tom@wickr.com>
2018-07-26 12:01:16 -0400

    Update node install script to support lib64 directories

--- node_install.sh
+++ node_install.sh
@@ -3,3 +3,10 @@ cd node/build
 cmake -DBUILD_NODE=ON -DCMAKE_POSITION_INDEPENDENT_CODE=true -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=../ ../../ 
 make
 make install
+
+cd ..
+
+if [ ! -d lib ] && [ -d lib64 ]; then
+    mkdir lib && cd lib
+    ln -s ../lib64/wickrcrypto.node wickrcrypto.node
+fi
