7e4d918f28f7578a429387820773a84ce825fb34
Tom Leavy <tom@wickr.com>
2018-09-17 14:44:37 -0400

    Force node to always install to lib folder

--- node_install.sh
+++ node_install.sh
@@ -1,11 +1,7 @@
 mkdir -p node/build
 cd node/build
-cmake -DBUILD_NODE=ON -DCMAKE_POSITION_INDEPENDENT_CODE=true -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=../ ../../ 
+cmake -DBUILD_NODE=ON -DCMAKE_INSTALL_LIBDIR='lib' -DCMAKE_POSITION_INDEPENDENT_CODE=true -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=../ ../../ 
 make
 make install
 
 cd ..
-
-if [ ! -d lib ] && [ -d lib64 ]; then
-    ln -s lib64 lib
-fi
