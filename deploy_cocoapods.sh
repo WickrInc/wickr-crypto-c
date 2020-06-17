7338225e3278cf974938da9cfc655858d4b5b168
Tom Leavy <tom@wickr.com>
2020-06-17 10:48:41 -0400

    Update CI Configuration

--- deploy_cocoapods.sh
+++ deploy_cocoapods.sh
@@ -1,18 +1,7 @@
-
 # usage ./deploy_cocoapods podreponame
-
-rm -rf pod_deploy
-mkdir pod_deploy
-cp WickrCryptoC.podspec pod_deploy/
-cd pod_deploy
 
 # Push std library version
 pod repo push $1 WickrCryptoC.podspec --allow-warnings --private --verbose 
 
-sed -i '' -e '19s/\([0-9]\{1,2\}\.[0-9]\{1,2\}\.[0-9]\{1,2\}\)/\1.fips/' WickrCryptoC.podspec
-sed -i '' -e 's/\.\/build/FIPS=true\ \.\/build/' WickrCryptoC.podspec
-sed -i '' -e 's/NO_FIPS/FIPS/' WickrCryptoC.podspec
-
 # Push FIPS library version
-pod repo push $1 WickrCryptoC.podspec --allow-warnings --private --verbose 
-
+pod repo push $1 WickrCryptoCFips.podspec --allow-warnings --private --verbose 
