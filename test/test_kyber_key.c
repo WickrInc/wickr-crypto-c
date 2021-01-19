da0b1051238fca39a7c02ff7c4332c0b657e24bb
Tom Leavy <tom@wickr.com>
2021-01-19 13:28:48 -0500

    WIP: Test file

---
+++ test/test_kyber_key.c
@@ -0,0 +1,20 @@
+
+#include "test_kyber_key.h"
+
+DESCRIBE(kyber_public_key, "kyber public keys")
+{
+    
+}
+END_DESCRIBE
+
+DESCRIBE(kyber_secret_key, "kyber secret keys")
+{
+    
+}
+END_DESCRIBE
+
+DESCRIBE(kyber_keypair, "kyber keypairs")
+{
+    
+}
+END_DESCRIBE
---
+++ test/test_kyber_key.h
@@ -0,0 +1,11 @@
+
+#ifndef test_kyber_key_h
+#define test_kyber_key_h
+
+#include <cspec.h>
+
+DEFINE_DESCRIPTION(kyber_public_key);
+DEFINE_DESCRIPTION(kyber_secret_key);
+DEFINE_DESCRIPTION(kyber_keypair)
+
+#endif /* test_kyber_key_h */
