659fbd2f692a43853cabb58ddf798117233b9ee7
Tom Leavy <tom@wickr.com>
2021-05-13 11:46:34 -0400

    Updated KDF swig interface to be more flexible

--- build_php_docker.sh
+++ build_php_docker.sh
@@ -2,7 +2,7 @@ docker build --quiet -t crypto-php-ubuntu-focal -f docker/ubuntu_focal_php/Docke
 
 docker run \
     --name crypto-php-ubuntu-focal-instance -it crypto-php-ubuntu-focal \
-    /bin/sh -c "mkdir build && cd build && cmake -DCMAKE_BUILD_TYPE=Release -DBUILD_PHP=ON -DCMAKE_POSITION_INDEPENDENT_CODE=ON -DBUILD_TESTS=ON .. && make && ctest"
+    /bin/sh -c "mkdir build && cd build && cmake -DCMAKE_BUILD_TYPE=Release -DBUILD_PHP=ON -DCMAKE_POSITION_INDEPENDENT_CODE=ON -DBUILD_TESTS=ON .. && make && CTEST_OUTPUT_ON_FAILURE=1 ctest"
 if [ "$?" != "0" ]
 then
     exit $?
